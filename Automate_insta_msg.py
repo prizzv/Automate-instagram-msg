bd06b32af979d979821555209ce3743edad3f775
Shashank <srkota1951@gmail.com>
2022-07-28 00:24:18 +0530

    Fixed the frontend

--- Automate_insta_msg.py
+++ Automate_insta_msg.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from tkinter import *
+from tkinter import ttk
+import pyautogui
+
 
 root = Tk()
 
@@ -16,7 +19,6 @@ root.geometry("1280x720")  #just to give a size
 
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
 
 users = ['']  # Add the users which you want to send message to 
 message = "Hey"   # Enter the message you want to send
@@ -47,18 +49,20 @@ class bot:
         time.sleep(4)
 
         # turn on notifications popup
-        self.bot.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]').click()  
-                            # /html/body/div[5]/div/div/div/div[3]/button[2]   xpath for not now
+        self.bot.find_element_by_xpath('/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[3]/button[2]').click()  
+                            # /html/body/div[5]/div/div/div/div[3]/button[2]   xpath for not now        
+
                             # /html/body/div[5]/div/div/div/div[3]/button[1]   xpath for turn on
         time.sleep(1)
 
         # the anchor element has to be selected and not just the svg           
-        x_mesenger ='//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/div[2]/a'             #  mesenger xpath
-        x_DM = '//*[@id="react-root"]/section/div/div[1]/div/div[3]/div/div[2]/a/svg'                  #  dm xpaths this is the old instagram xpath 
+        x_mesenger ='/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a'             #  mesenger xpath
+        # x_DM = '//*[@id="react-root"]/section/div/div[1]/div/div[3]/div/div[2]/a/svg'                  #  dm xpaths this is the old instagram xpath 
         self.bot.find_element_by_xpath(x_mesenger).click()   # depending on the account choose xpaths default/NewUser is of DM xpath            
         time.sleep(2)
 
-        # pencil/new message button
+        # pencil/new message button  
+        # ---------------------not working fromn here -------------------
         self.bot.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button/div').click()
         time.sleep(1)
 
@@ -89,34 +93,41 @@ class bot:
             self.bot.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button').click()
             time.sleep(1)
 
+
+
 lbl1 = Label(root, text = "Enter your username:   ")
 lbl1.grid()
 
-username = Entry(root, width = 10)  
-username.grid(column =1, row =0)
 
-lbl2 = Label(root, text = "Enter your password:   ")
-lbl2.grid(column=0, row=1)
+usernameInput = Entry(root, width = 35)
+usernameInput.grid(column =1, row =0)
 
-password = Entry(root, width = 10)  
-password.grid(column =1, row =1)
+lbl2 = Label(root, text = "Enter your password:   ").grid(column=0, row=1)
+
+passwordInput = Entry(root, width = 35)
+passwordInput.grid(column =1, row =1)
 
 def clicked():
-    
-    return password, username
+    global driver, username, password
+
+    username = usernameInput.get()    
+    password = passwordInput.get()
+
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    root.destroy()
 
 btn = Button(root, text = "Enter", fg = "blue", bg = "black", command=clicked)
 btn.grid(column = 0, row = 2)
 
-username1 = input("Enter your username ")
-password2 = input("Enter your password ")
 
+root.mainloop()
+
+username = "shashaank213@gmail.com"
+password = "aniso9001"
 
 
 bot(username, password, users, message)
 
 input("DONE")
 
-
-
-root.mainloop()
